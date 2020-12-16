beb0e36f3d4e1d28c9d088ca1f55ef83b782e1c9
HydroxideX <Yahiaelsaadawy@gmail.com>
2020-12-17 00:20:27 +0200

    removed thread_yield()

--- devices/timer.c
+++ devices/timer.c
@@ -93,7 +93,6 @@ timer_sleep (int64_t ticks)
 
   ASSERT (intr_get_level () == INTR_ON);
   while (timer_elapsed (start) < ticks) 
-    thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
